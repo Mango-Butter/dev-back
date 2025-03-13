a5c3030fbf53b31a3c48d3f3daf06bb8d93a0c32
hyunzzii <yhynym@gmail.com>
2025-03-13 15:15:25 +0900

    Initial commit

---
+++ src/main/java/com/mangobutter/dev_back/DevBackApplication.java
@@ -0,0 +1,13 @@
+package com.mangobutter.dev_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mangobutter/dev_back/DevBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangobutter.dev_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
