e6fa244d61aefc5ca4a6968c5b53f6f72dc13e3d
심재엽 <jysim0326@ajou.ac.kr>
2025-04-14 21:05:59 +0900

    test: TestController 위치 이동 및 404 not found 원인 확인 목적

--- app/src/main/java/com/mangoboss/app/api/controller/TestController.java
+++ app/src/main/java/com/mangoboss/app/api/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.mangoboss.app;
+package com.mangoboss.app.api.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
