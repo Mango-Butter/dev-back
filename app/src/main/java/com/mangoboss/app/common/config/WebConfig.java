5b408b6a2d5b80e550356657123128a483b4b8c4
심재엽 <jysim0326@ajou.ac.kr>
2025-04-14 22:53:07 +0900

    chore: CORS 설정에 허용 도메인 추가 (localhost, test/dev/prod 프론트)

--- app/src/main/java/com/mangoboss/app/common/config/WebConfig.java
+++ app/src/main/java/com/mangoboss/app/common/config/WebConfig.java
@@ -12,11 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOriginPatterns(
-				"*"
-				// "http://localhost:5173",
-				// "https://dr2f24xo5uydt.cloudfront.net", // 프론트 test
-				// "https://dx44qcj8tqeon.cloudfront.net", // 프론트 dev
-				// "https://d3741u3vzg4n3d.cloudfront.net" // 프론트 prod
+				"http://localhost:5173",
+				"https://dr2f24xo5uydt.cloudfront.net", // 프론트 test
+				"https://d3741u3vzg4n3d.cloudfront.net", // 프론트 dev
+				"https://dx44qcj8tqeon.cloudfront.net" // 프론트 prod
 			)
 			.allowedMethods("*")
 			.allowedHeaders("*")
