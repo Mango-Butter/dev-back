c51c39c7cb854472f341a2224cc8677fadb1bdbe
hyunzzii <yhynym@gmail.com>
2025-04-20 22:49:00 +0900

    feat: staff 쿼리 구현 (#9)

---
+++ app/src/main/java/com/mangoboss/app/infra/persistence/StaffRepositoryImpl.java
@@ -0,0 +1,17 @@
+package com.mangoboss.app.infra.persistence;
+
+import com.mangoboss.app.domain.repository.StaffRepository;
+import com.mangoboss.storage.staff.StaffEntity;
+import com.mangoboss.storage.staff.StaffJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class StaffRepositoryImpl implements StaffRepository {
+    private final StaffJpaRepository staffJpaRepository;
+
+    public StaffEntity save(final StaffEntity staff){
+        return staffJpaRepository.save(staff);
+    }
+}
