e89f9a980aa14c0573ddfe7024e235f83973a7bd
hyunzzii <yhynym@gmail.com>
2025-05-01 12:08:29 +0900

    fix: data.sql에 staff 안들어간 것 수정 (#18)

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -6,10 +6,10 @@ VALUES (1, 'test1@ajou.ac.kr', '망사장', null, '010-1234-5678', '11111111', '
        (2, 'test2@ajou.ac.kr', '망알바', null, '010-2312-1111', '22222222', '2006-05-11',
         'https://mblogthumb-phinf.pstatic.net/MjAyMDAyMTBfODAg/MDAxNTgxMzA0MTE3ODMy.ACRLtB9v5NH-I2qjWrwiXLb7TeUiG442cJmcdzVum7cg.eTLpNg_n0rAS5sWOsofRrvBy0qZk_QcWSfUiIagTfd8g.JPEG.lattepain/1581304118739.jpg?type=w800',
         'STAFF', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       (3, 'test3@ajou.ac.kr', '배고파', null, '010-4972-4844', '33333333', '2004-07-22',
+       (3, 'test3@ajou.ac.kr', '망알바2', null, '010-4972-4844', '33333333', '2004-07-22',
         'https://mblogthumb-phinf.pstatic.net/MjAyMDAyMTBfODAg/MDAxNTgxMzA0MTE3ODMy.ACRLtB9v5NH-I2qjWrwiXLb7TeUiG442cJmcdzVum7cg.eTLpNg_n0rAS5sWOsofRrvBy0qZk_QcWSfUiIagTfd8g.JPEG.lattepain/1581304118739.jpg?type=w800',
         'STAFF', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       (4, 'test4@ajou.ac.kr', '파스타', null, '010-3993-2222', '44444444', '2005-11-30',
+       (4, 'test4@ajou.ac.kr', '망알바3', null, '010-3993-2222', '44444444', '2005-11-30',
         'https://mblogthumb-phinf.pstatic.net/MjAyMDAyMTBfODAg/MDAxNTgxMzA0MTE3ODMy.ACRLtB9v5NH-I2qjWrwiXLb7TeUiG442cJmcdzVum7cg.eTLpNg_n0rAS5sWOsofRrvBy0qZk_QcWSfUiIagTfd8g.JPEG.lattepain/1581304118739.jpg?type=w800',
         'STAFF', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
@@ -20,5 +20,11 @@ VALUES (1, 1, '망고쥬스', '경기도 수원시 영통구 월드컵로 206', 
 
 INSERT INTO staff (staff_id, user_id, store_id, name, profile_image_url, created_at, modified_at)
 VALUES (1, 2, 1, '망알바',
+        'https://mblogthumb-phinf.pstatic.net/MjAyMDAyMTBfODAg/MDAxNTgxMzA0MTE3ODMy.ACRLtB9v5NH-I2qjWrwiXLb7TeUiG442cJmcdzVum7cg.eTLpNg_n0rAS5sWOsofRrvBy0qZk_QcWSfUiIagTfd8g.JPEG.lattepain/1581304118739.jpg?type=w800',
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (2, 3, 1, '망알바2',
+        'https://mblogthumb-phinf.pstatic.net/MjAyMDAyMTBfODAg/MDAxNTgxMzA0MTE3ODMy.ACRLtB9v5NH-I2qjWrwiXLb7TeUiG442cJmcdzVum7cg.eTLpNg_n0rAS5sWOsofRrvBy0qZk_QcWSfUiIagTfd8g.JPEG.lattepain/1581304118739.jpg?type=w800',
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (3, 4, 1, '망알바3',
         'https://mblogthumb-phinf.pstatic.net/MjAyMDAyMTBfODAg/MDAxNTgxMzA0MTE3ODMy.ACRLtB9v5NH-I2qjWrwiXLb7TeUiG442cJmcdzVum7cg.eTLpNg_n0rAS5sWOsofRrvBy0qZk_QcWSfUiIagTfd8g.JPEG.lattepain/1581304118739.jpg?type=w800',
         CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
