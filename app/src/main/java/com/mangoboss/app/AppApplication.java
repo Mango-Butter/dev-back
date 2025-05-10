761a116370ea87963ba9400b29696af40f0cad5d
hyunzzii <yhynym@gmail.com>
2025-05-11 00:07:45 +0900

    fix: JpaEntity 로드 오류 해결 (#27)
    - 배치 모듈 추가 후, App 모듈이 JpaEntity를 못 읽는 오류 발생
    - 스캔 범위 명시하여 해결

--- app/src/main/java/com/mangoboss/app/AppApplication.java
+++ app/src/main/java/com/mangoboss/app/AppApplication.java
@@ -2,9 +2,13 @@ package com.mangoboss.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.mangoboss.storage")
+@EntityScan(basePackages = "com.mangoboss.storage")
 @SpringBootApplication(scanBasePackages = {"com.mangoboss.app","com.mangoboss.storage"})
 public class AppApplication {
 
