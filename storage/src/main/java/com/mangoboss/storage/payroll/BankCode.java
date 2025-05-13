1668936b761d7a2b79ad3693ab27e47ab1d94b38
hyunzzii <yhynym@gmail.com>
2025-05-14 00:40:48 +0900

    feat: 개선된 급여 설정 및 계좌 관리 엔티티 추가 (#20) - 급여 설정 엔티티 및 연관 관계 추가 - public -> private 으로 생성자 변경

--- storage/src/main/java/com/mangoboss/storage/payroll/BankCode.java
+++ storage/src/main/java/com/mangoboss/storage/payroll/BankCode.java
@@ -10,17 +10,17 @@ public enum BankCode {
     NH("농협은행", "011"),
     NH_SANGHO("농협상호금융", "012");
 
-    private final String name;
+    private final String displayName;
     private final String code;
 
     BankCode(final String name, final String code) {
-        this.name = name;
+        this.displayName = name;
         this.code = code;
     }
 
     public static Optional<BankCode> findCodeByName(final String name) {
         return Arrays.stream(BankCode.values())
-                .filter(b -> b.name.equals(name))
+                .filter(b -> b.displayName.equals(name))
                 .findFirst();
     }
 }
