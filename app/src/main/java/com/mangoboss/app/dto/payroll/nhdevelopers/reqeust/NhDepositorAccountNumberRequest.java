477bc1e90011ad60d840cba86d421040fb9dad7e
hyunzzii <yhynym@gmail.com>
2025-05-14 16:04:00 +0900

    style: 클래스명 NH → Nh로 변경 (#20)

--- app/src/main/java/com/mangoboss/app/dto/payroll/nhdevelopers/reqeust/NhDepositorAccountNumberRequest.java
+++ app/src/main/java/com/mangoboss/app/dto/payroll/nhdevelopers/reqeust/NhDepositorAccountNumberRequest.java
@@ -3,14 +3,14 @@ package com.mangoboss.app.dto.payroll.nhdevelopers.reqeust;
 import lombok.Builder;
 
 @Builder
-public record NHDepositorAccountNumberRequest(
+public record NhDepositorAccountNumberRequest(
         NhCommonPartHeaderRequest Header,
         String Bncd,
         String Acno
 ) {
 
-    public static NHDepositorAccountNumberRequest create(final NhCommonPartHeaderRequest header, final String bncd, final String acno) {
-        return NHDepositorAccountNumberRequest.builder()
+    public static NhDepositorAccountNumberRequest create(final NhCommonPartHeaderRequest header, final String bncd, final String acno) {
+        return NhDepositorAccountNumberRequest.builder()
                 .Header(header)
                 .Bncd(bncd)
                 .Acno(acno)
