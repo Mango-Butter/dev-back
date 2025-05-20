43ed62e68e7ebc2b02faeab903d32291804019c3
심재엽 <jysim0326@ajou.ac.kr>
2025-05-21 00:36:29 +0900

    feat: 알바생 전체 근무 일정 조회 API 개발 (#58)

--- app/src/main/java/com/mangoboss/app/api/facade/calendar/StaffCalendarFacade.java
+++ app/src/main/java/com/mangoboss/app/api/facade/calendar/StaffCalendarFacade.java
@@ -1,8 +1,8 @@
-package com.mangoboss.app.api.facade.calender;
+package com.mangoboss.app.api.facade.calendar;
 
 import com.mangoboss.app.domain.service.attendance.AttendanceService;
 import com.mangoboss.app.domain.service.schedule.ScheduleService;
-import com.mangoboss.app.domain.service.store.StoreService;
+import com.mangoboss.app.domain.service.staff.StaffService;
 import com.mangoboss.app.dto.calender.WorkWithStaffResponse;
 import com.mangoboss.app.dto.calender.WorkDotResponse;
 import com.mangoboss.storage.attendance.projection.WorkDotProjection;
@@ -15,19 +15,19 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class BossCalenderFacade {
+public class StaffCalendarFacade {
     private final AttendanceService attendanceService;
     private final ScheduleService scheduleService;
-    private final StoreService storeService;
+    private final StaffService staffService;
 
-    public List<WorkDotResponse> getWorkDots(final Long storeId, final Long bossId, final LocalDate start, final LocalDate end) {
-        storeService.isBossOfStore(storeId, bossId);
+    public List<WorkDotResponse> getWorkDots(final Long storeId, final Long userId, final LocalDate start, final LocalDate end) {
+        staffService.getVerifiedStaff(userId, storeId);
         List<WorkDotProjection> workDotProjections = attendanceService.getWorkDots(storeId, start, end);
         return workDotProjections.stream().map(WorkDotResponse::of).toList();
     }
 
-    public List<WorkWithStaffResponse> getDailyWorks(final Long storeId, final Long bossId, final LocalDate date) {
-        storeService.isBossOfStore(storeId, bossId);
+    public List<WorkWithStaffResponse> getDailyWorks(final Long storeId, final Long userId, final LocalDate date) {
+        staffService.getVerifiedStaff(userId, storeId);
         List<ScheduleEntity> schedules = scheduleService.getDailySchedules(storeId, date);
         return schedules.stream().map(WorkWithStaffResponse::of).toList();
     }
