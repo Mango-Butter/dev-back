8e61c036fadb8423ee7edb0507a380904623b598
심재엽 <jysim0326@ajou.ac.kr>
2025-05-27 02:36:19 +0900

    feat: 사장이 날짜별 업무 조회 기능 개발 (#70) - 날짜별 업무 + 완료한 업무 현황 조회

--- app/src/main/java/com/mangoboss/app/api/controller/task/BossTaskController.java
+++ app/src/main/java/com/mangoboss/app/api/controller/task/BossTaskController.java
@@ -2,14 +2,18 @@ package com.mangoboss.app.api.controller.task;
 
 import com.mangoboss.app.api.facade.task.BossTaskFacade;
 import com.mangoboss.app.common.security.CustomUserDetails;
+import com.mangoboss.app.dto.ListWrapperResponse;
 import com.mangoboss.app.dto.task.request.SingleTaskCreateRequest;
 import com.mangoboss.app.dto.s3.response.UploadPreSignedUrlResponse;
 import com.mangoboss.app.dto.task.request.TaskRoutineCreateRequest;
+import com.mangoboss.app.dto.task.response.AssignedTaskResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/boss/stores/{storeId}/tasks")
@@ -42,5 +46,13 @@ public class BossTaskController {
                                                         @RequestParam final String contentType) {
         final Long userId = userDetails.getUserId();
         return bossTaskFacade.generateReferenceImageUploadUrl(storeId, userId, extension, contentType);
+    }
+
+    @GetMapping
+    public ListWrapperResponse<AssignedTaskResponse> getTasksByDate(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                                    @PathVariable final Long storeId,
+                                                                    @RequestParam final LocalDate date) {
+        final Long userId = userDetails.getUserId();
+        return ListWrapperResponse.of(bossTaskFacade.getTasksByDate(storeId, userId, date));
     }
 }
--- app/src/main/java/com/mangoboss/app/api/facade/task/BossTaskFacade.java
+++ app/src/main/java/com/mangoboss/app/api/facade/task/BossTaskFacade.java
@@ -2,19 +2,28 @@ package com.mangoboss.app.api.facade.task;
 
 import com.mangoboss.app.common.constant.S3FileType;
 import com.mangoboss.app.common.util.S3FileManager;
+import com.mangoboss.app.domain.service.staff.StaffService;
 import com.mangoboss.app.domain.service.task.TaskService;
 import com.mangoboss.app.domain.service.store.StoreService;
 import com.mangoboss.app.domain.service.task.context.TaskRoutineStrategyContext;
 import com.mangoboss.app.domain.service.task.strategy.TaskRoutineGenerationStrategy;
+import com.mangoboss.app.dto.staff.response.StaffSimpleResponse;
 import com.mangoboss.app.dto.task.request.SingleTaskCreateRequest;
 import com.mangoboss.app.dto.s3.response.UploadPreSignedUrlResponse;
 import com.mangoboss.app.dto.task.request.TaskRoutineCreateRequest;
+import com.mangoboss.app.dto.task.response.AssignedTaskResponse;
+import com.mangoboss.app.dto.task.response.TaskLogDetailResponse;
 import com.mangoboss.storage.task.TaskEntity;
+import com.mangoboss.storage.task.TaskLogEntity;
 import com.mangoboss.storage.task.TaskRoutineEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
@@ -22,6 +31,7 @@ public class BossTaskFacade {
 
     private final TaskService taskService;
     private final StoreService storeService;
+    private final StaffService staffService;
     private final S3FileManager s3FileManager;
     private final TaskRoutineStrategyContext taskRoutineStrategyContext;
 
@@ -46,5 +56,37 @@ public class BossTaskFacade {
         storeService.isBossOfStore(storeId, bossId);
         final String key = s3FileManager.generateFileKey(S3FileType.TASK_REFERENCE, extension);
         return s3FileManager.generateUploadPreSignedUrl(key, contentType);
+    }
+
+    public List<AssignedTaskResponse> getTasksByDate(final Long storeId, final Long bossId, final LocalDate date) {
+        storeService.isBossOfStore(storeId, bossId);
+
+        final List<TaskEntity> tasks = taskService.getTasksByDate(storeId, date);
+        if (tasks.isEmpty()) {
+            return List.of();
+        }
+
+        final List<Long> taskIds = tasks.stream().map(TaskEntity::getId).toList();
+        final List<TaskLogEntity> taskLogs = taskService.getTaskLogsByTaskIds(taskIds);
+        final Map<Long, TaskLogEntity> logMap = taskLogs.stream()
+                .collect(Collectors.toMap(log -> log.getTask().getId(), log -> log));
+
+        return tasks.stream()
+                .map(task -> {
+                    final TaskLogEntity log = logMap.get(task.getId());
+
+                    final TaskLogDetailResponse taskLog = (log != null)
+                            ? TaskLogDetailResponse.of(
+                            log.getTaskLogImageUrl(),
+                            log.getCreatedAt(),
+                            StaffSimpleResponse.fromEntity(
+                                    staffService.getStaffById(log.getStaffId())
+                            )
+                    )
+                            : null;
+
+                    return AssignedTaskResponse.of(task, taskLog);
+                })
+                .toList();
     }
 }
